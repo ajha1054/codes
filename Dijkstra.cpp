25976e9aee11b19804431079a6feda9da6af5b5c
ajha1054 <55062327+ajha1054@users.noreply.github.com>
2020-04-24 10:34:46 +0530

    Add files via upload

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,55 +1,145 @@
-#include <queue>
-#include <vector>
-#include <cstdio>
 #include <iostream>
+#include <limits.h>
+
 using namespace std;
-#define INF 10000010
-vector<pair<int, int>> graph[5 * 100001];
-int dis[5 * 100001];
-int dij(vector<pair<int, int>> *v, int s, int *dis)
+
+//Wrapper class for storing a graph
+class Graph
 {
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    // source distance to zero.
-    pq.push(make_pair(0, s));
-    dis[s] = 0;
-    int u;
-    while (!pq.empty())
+public:
+    int vertexNum;
+    int **edges;
+
+    //Constructs a graph with V vertices and E edges
+    Graph(const int V)
     {
-        u = (pq.top()).second;
-        pq.pop();
-        for (vector<pair<int, int>>::iterator it = v[u].begin(); it != v[u].end(); it++)
+
+        // initializes the array edges.
+        this->edges = new int *[V];
+        for (int i = 0; i < V; i++)
         {
-            if (dis[u] + it->first < dis[it->second])
+            edges[i] = new int[V];
+        }
+
+        // fills the array with zeros.
+        for (int i = 0; i < V; i++)
+        {
+            for (int j = 0; j < V; j++)
             {
-                dis[it->second] = dis[u] + it->first;
-                pq.push(make_pair(dis[it->second], it->second));
+                edges[i][j] = 0;
+            }
+        }
+
+        this->vertexNum = V;
+    }
+
+    //Adds the given edge to the graph
+    void addEdge(int src, int dst, int weight)
+    {
+        this->edges[src][dst] = weight;
+    }
+};
+//Utility function to find minimum distance vertex in mdist
+int minDistance(int mdist[], bool vset[], int V)
+{
+    int minVal = INT_MAX, minInd = 0;
+    for (int i = 0; i < V; i++)
+    {
+        if (!vset[i] && (mdist[i] < minVal))
+        {
+            minVal = mdist[i];
+            minInd = i;
+        }
+    }
+
+    return minInd;
+}
+
+//Utility function to print distances
+void print(int dist[], int V)
+{
+    cout << "\nVertex  Distance" << endl;
+    for (int i = 0; i < V; i++)
+    {
+        if (dist[i] < INT_MAX)
+            cout << i << "\t" << dist[i] << endl;
+        else
+            cout << i << "\tINF" << endl;
+    }
+}
+
+//The main function that finds the shortest path from given source
+//to all other vertices using Dijkstra's Algorithm.It doesn't work on negative
+//weights
+void Dijkstra(Graph graph, int src)
+{
+    int V = graph.vertexNum;
+    int mdist[V]; //Stores updated distances to vertex
+    bool vset[V]; // vset[i] is true if the vertex i included
+    // in the shortest path tree
+
+    //Initialise mdist and vset. Set distance of source as zero
+    for (int i = 0; i < V; i++)
+    {
+        mdist[i] = INT_MAX;
+        vset[i] = false;
+    }
+
+    mdist[src] = 0;
+
+    //iterate to find shortest path
+    for (int count = 0; count < V - 1; count++)
+    {
+        int u = minDistance(mdist, vset, V);
+
+        vset[u] = true;
+
+        for (int v = 0; v < V; v++)
+        {
+            if (!vset[v] && graph.edges[u][v] && mdist[u] + graph.edges[u][v] < mdist[v])
+            {
+                mdist[v] = mdist[u] + graph.edges[u][v];
             }
         }
     }
+
+    print(mdist, V);
 }
+
+//Driver Function
 int main()
 {
-    int m, n, l, x, y, s;
-    // n--> number of nodes , m --> number of edges
-    cin >> n >> m;
-    for (int i = 0; i < m; i++)
+    int V, E, gsrc;
+    int src, dst, weight;
+    cout << "Enter number of vertices: ";
+    cin >> V;
+    cout << "Enter number of edges: ";
+    cin >> E;
+    Graph G(V);
+    for (int i = 0; i < E; i++)
     {
-        // input edges.
-        scanf("%d%d%d", &x, &y, &l);
-        graph[x].push_back(make_pair(l, y));
-        graph[y].push_back(make_pair(l, x)); // comment this line for directed graph
-    }
-    // start node.
-    scanf("%d", &s);
-    // intialise all distances to infinity.
-    for (int i = 1; i <= n; i++)
-        dis[i] = INF;
-    dij(graph, s, dis);
+        cout << "\nEdge " << i + 1 << "\nEnter source: ";
+        cin >> src;
+        cout << "Enter destination: ";
+        cin >> dst;
+        cout << "Enter weight: ";
+        cin >> weight;
 
-    for (int i = 1; i <= n; i++)
-        if (dis[i] == INF)
-            cout << "-1 ";
+        // makes sure source and destionation are in the proper bounds.
+        if (src >= 0 && src < V && dst >= 0 && dst < V)
+        {
+            G.addEdge(src, dst, weight);
+        }
         else
-            cout << dis[i] << " ";
+        {
+            cout << "source and/or destination out of bounds" << endl;
+            i--;
+            continue;
+        }
+    }
+    cout << "\nEnter source:";
+    cin >> gsrc;
+    Dijkstra(G, gsrc);
+
     return 0;
 }
